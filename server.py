7a468466b9ab350e49ec16759eddca2c96b9bc10
mukti222 <135799527+mukti222@users.noreply.github.com>
2024-02-21 23:13:36 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def query2(sql, param):
     conn.commit()
     cur.close()
     conn.close()
-### tambahan 1
+#### tambahan 1
 
 
 ############################################ rute untuk transfer data ###########################################
@@ -38,6 +38,7 @@ def data_jadwal():
     Getdata2 = query("select * from t_jadwal")
     return json.dumps(Getdata2)
 ###tambahan 1
+################# save edit #####################
 @post('/save_mahasiswa')
 def save_mahasiswa():
     ###############  ambil data input form  ###################
@@ -73,37 +74,6 @@ def save_mahasiswa():
                 )
             hasil = {'success': True, 'pesan': 'Data Berhasil Disimpan'}
     return json.dumps(hasil)
-@post('/hapus_mahasiswa')
-def hapus_mahasiswa():
-    id = int(request.forms.get('id'))
-    nim = request.forms.get('nim')
-    nama = request.forms.get('nama')
-    if nim == "" or nim == None:
-        hasil = {'success': False, 'pesan': 'Data gagal dihapus karena nim masih kosong'}
-    elif nama == "" or nama == None:
-        hasil = {'success': False, 'pesan': 'Data gagal dihapus karena nama masih kosong'}
-    else:
-        query2("delete from t_mahasiswa where id=%(int)s", {'int': id})
-        hasil = {'success': True, 'pesan': 'Data ('+nama+') Berhasil Dihapus'}
-    return json.dumps(hasil)
-@post('/hapus_fakultas')
-def hapus_fakultas():
-    id = int(request.forms.get('id'))
-    nama_fakultas = request.forms.get('nama_fakultas')
-    # nama = request.forms.get('nama')
-    if nama_fakultas == "" or nama_fakultas == None:
-        hasil = {'success': False, 'pesan': 'Data gagal dihapus karena fakultas masih kosong'}
-    # elif nama == "" or nama == None:
-    #     hasil = {'success': False, 'pesan': 'Data gagal dihapus karena nama masih kosong'}
-    else:
-        query2("delete from t_fakultas where id=%(int)s", {'int': id})
-        hasil = {'success': True, 'pesan': 'Data ('+nama_fakultas+') Berhasil Dihapus'}
-    return json.dumps(hasil)
-###tambahan 1
-
-
-####tambahan 1
-# percobaan ke save_fakultas
 @post('/save_fakultas')
 def save_fakultas():
     ###############  ambil data input form  ###################
@@ -136,8 +106,87 @@ def save_fakultas():
                 )
             hasil = {'success': True, 'pesan': 'Data Berhasil Disimpan'}
     return json.dumps(hasil)
-####tambahan 1
+@post('/save_jadwal')
+def save_jadwal():
+    ###############  ambil data input form  ###################
+    id = int(request.forms.get('input_id'))
+    kode_matkul = request.forms.get('input_kode_matkul')
+    print(kode_matkul)
+    nama_matkul = request.forms.get('input_nama_matkul')
+    kode_dosen = request.forms.get('input_kode_dosen')
+    nama_dosen = request.forms.get('input_nama_dosen')
 
+    ############### pengecekan isi form FAKULTAS
+    if kode_matkul == "" or kode_matkul == None:
+        hasil = {'success': False, 'pesan': 'Data gagal diset karena kode_matkul masih kosong'}
+    elif nama_matkul == "" or nama_matkul == None:
+        hasil = {'success': False, 'pesan': 'Data gagal diset karena nama_matkul masih kosong'}
+    elif kode_dosen == "" or kode_dosen == None:
+        hasil = {'success': False, 'pesan': 'Data gagal diset karena kode_dosen masih kosong'}
+    elif nama_dosen == "" or nama_dosen == None:
+        hasil = {'success': False, 'pesan': 'Data gagal diset karena nama_dosen masih kosong'}
+    else:
+        ############ dibawah 0 ############ logika apakah run insert atau update ####
+        if id > 0:
+            ##########  memakai query psycopg
+            query2(
+                "UPDATE t_jadwal SET kode_matkul=%s, nama_matkul=%s, kode_dosen=%s, nama_dosen=%s WHERE id=%s;",
+                (kode_matkul, nama_matkul, kode_dosen, nama_dosen, id)
+                )
+            hasil = {'success': True, 'pesan': 'Data Berhasil Disimpan'}
+        else:
+            query2(
+                "insert into t_jadwal(kode_matkul, nama_matkul, kode_dosen, nama_dosen) values(%s, %s, %s, %s)",
+                (kode_matkul, nama_matkul, kode_dosen, nama_dosen)
+                )
+            hasil = {'success': True, 'pesan': 'Data Berhasil Disimpan'}
+    return json.dumps(hasil)
+
+##################  hapus   ######################
+@post('/hapus_mahasiswa')
+def hapus_mahasiswa():
+    id = int(request.forms.get('id'))
+    nim = request.forms.get('nim')
+    nama = request.forms.get('nama')
+    if nim == "" or nim == None:
+        hasil = {'success': False, 'pesan': 'Data gagal dihapus karena nim masih kosong'}
+    elif nama == "" or nama == None:
+        hasil = {'success': False, 'pesan': 'Data gagal dihapus karena nama masih kosong'}
+    else:
+        query2("delete from t_mahasiswa where id=%(int)s", {'int': id})
+        hasil = {'success': True, 'pesan': 'Data ('+nama+') Berhasil Dihapus'}
+    return json.dumps(hasil)
+@post('/hapus_fakultas')
+def hapus_fakultas():
+    id = int(request.forms.get('id'))
+    nama_fakultas = request.forms.get('nama_fakultas')
+    # nama = request.forms.get('nama')
+    if nama_fakultas == "" or nama_fakultas == None:
+        hasil = {'success': False, 'pesan': 'Data gagal dihapus karena fakultas masih kosong'}
+    # elif nama == "" or nama == None:
+    #     hasil = {'success': False, 'pesan': 'Data gagal dihapus karena nama masih kosong'}
+    else:
+        query2("delete from t_fakultas where id=%(int)s", {'int': id})
+        hasil = {'success': True, 'pesan': 'Data ('+nama_fakultas+') Berhasil Dihapus'}
+    return json.dumps(hasil)
+@post('/hapus_jadwal')
+def hapus_jadwal():
+    id = int(request.forms.get('id'))
+    kode_matkul = request.forms.get('kode_matkul')
+    # nama = request.forms.get('nama')
+    if kode_matkul == "" or kode_matkul == None:
+        hasil = {'success': False, 'pesan': 'Data gagal dihapus karena kode_matkul masih kosong'}
+    # elif nama == "" or nama == None:
+    #     hasil = {'success': False, 'pesan': 'Data gagal dihapus karena nama masih kosong'}
+    else:
+        query2("delete from t_jadwal where id=%(int)s", {'int': id})
+        hasil = {'success': True, 'pesan': 'Data ('+kode_matkul+') Berhasil Dihapus'}
+    return json.dumps(hasil)
+###tambahan 1
+# kode_matkul
+# nama_matkul
+# kode_dosen
+# nama_dosen
 
 ############################################ rute untuk halaman #############################################
 @route("/")
